4bcd901cb0421cc82452419bd6c06a6dd448dbc3
mssistemas01 <jlvpxz.9898@gmail.com>
2022-12-19 12:00:21 -0400

    heads

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var index = fs.readFileSync('index.html');
 var control = fs.readFileSync('conexion.html');
 
   var app = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
     if(req.url == "/control"){
         res.end(control);
     }else{
