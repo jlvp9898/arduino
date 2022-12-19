1fff8045cfa7e7550fdc90069d28985ab22e337c
mssistemas01 <jlvpxz.9898@gmail.com>
2022-12-19 12:15:06 -0400

    update path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var  fs  = require('fs');
-var index = fs.readFileSync('index.html');
-var control = fs.readFileSync('conexion.html');
+var index = fs.readFileSync('./index.html');
+var control = fs.readFileSync('./conexion.html');
 
   var app = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
