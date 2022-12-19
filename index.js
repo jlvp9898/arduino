c3b7724da5a672369b0f179585be70a684aee8f1
mssistemas01 <jlvpxz.9898@gmail.com>
2022-12-19 11:55:48 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var  fs  = require('fs');
+var index = fs.readFileSync('index.html');
+var control = fs.readFileSync('conexion.html');
+
+  var app = http.createServer(function(req, res){
+    if(req.url == "/control"){
+        res.end(control);
+    }else{
+        res.end(index);
+    }
+      
+  });
+
+  const { Server } = require("socket.io");
+  const io = new Server(app);
+
+  io.on('connection', function(socket){
+    console.log("Usuario conectado");
+    socket.on('led', function(data){
+      
+      socket.broadcast.emit("led", data);
+    });
+
+  });
+
+  app.listen(8080);
